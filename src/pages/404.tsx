039119f4e7fc095810bbe9e01602e2ee6ede6345
lunarmoon7 <zentechie7@gmail.com>
2024-04-16 09:11:17 +0900

    style: 스타일 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,9 +2,9 @@ import { Link } from "@/router";
 
 const NotFoundPage = () => {
   return (
-    <section className="flex h-full w-full flex-col items-center justify-center gap-3">
+    <section className="flex flex-col gap-3 justify-center items-center w-full h-full">
       <h1 className="text-7xl font-bold">404 Error</h1>
-      <Link to="/" className="border rounded-lg p-3 hover:bg-slate-100">Back to Home</Link>
+      <Link to="/" className="p-3 rounded-lg border hover:bg-slate-100">Back to Home</Link>
     </section>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-const Home = () =>{
+const Home = () => {
   return (
-    <section className="flex h-full w-full flex-col items-center justify-center gap-3">
+    <section className="flex flex-col gap-3 justify-center items-center w-full h-full">
       <h1 className="text-8xl font-bold">Uket</h1>
       <h3 className="font-thin">we&apos;re working hard now</h3>
     </section>
   );
-}
+};
 
 export default Home;
