d8e82783db0900a4ae34e335ef4e5193d3e0d790
lunarmoon7 <zentechie7@gmail.com>
2024-04-20 19:00:42 +0900

    style: 아이템 클릭 시 배경색 변경하는 변수 추가

--- src/pages/signup/_components/UserTypeItem.tsx
+++ src/pages/signup/_components/UserTypeItem.tsx
@@ -1,17 +1,26 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
+import { cn } from "@/lib/utils";
+
 interface UserTypeItemProps {
   title: string;
   desc: string;
+  selected: boolean;
 }
 
 const UserTypeItem = (props: UserTypeItemProps) => {
-  const { title, desc } = props;
+  const { title, desc, selected } = props;
+  
   return (
-    <div className="flex flex-col items-center gap-2 bg-[#d7d7d7] px-5 pb-5 pt-3">
+    <div
+      className={cn(
+        `flex flex-col items-center gap-2 rounded-md px-5 pb-5 pt-3 transition-colors duration-300`,
+        `${selected ? "bg-brand/50" : "bg-[#d7d7d7]"}`,
+      )}
+    >
       <header>{title}</header>
-      <section className="flex items-center justify-evenly gap-5">
-        <Skeleton className="h-20 w-20 rounded-full" />
+      <section className="flex gap-5 justify-evenly items-center">
+        <Skeleton className="w-20 h-20 rounded-full" />
         <aside>{desc}</aside>
       </section>
     </div>
