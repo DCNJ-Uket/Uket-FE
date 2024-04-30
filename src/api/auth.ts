f74c55313c0a62077297e2d1cf2351623ccc3089
lunarmoon7 <zentechie7@gmail.com>
2024-04-30 13:52:38 +0900

    feat: 회원가입 함수 추가
    - 로그인 함수의 반환값을 수정했습니다.
    - userType에 따라 회원가입 시 전달하는 데이터를 분기 처리 했습니다.

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,11 +2,14 @@ import { AxiosError } from "axios";
 
 import { GOOGLE_REDIRECT_URI, KAKAO_REDIRECT_URI } from "@/constants/auth_url";
 
+import { LoginRequestParams } from "@/types/authType";
+
 import { instance } from "./instance";
 
-export type Provider = "kakao" | "google";
+import { FormSchemaType } from "@/pages/signup/_hooks/useStackForm";
 
-export const login = async (code: string, provider: Provider) => {
+// export interface SignUpData
+export const login = async ({ code, provider }: LoginRequestParams) => {
   const redirect_uri =
     provider === "google" ? GOOGLE_REDIRECT_URI : KAKAO_REDIRECT_URI;
 
@@ -16,9 +19,41 @@ export const login = async (code: string, provider: Provider) => {
       redirectUri: redirect_uri,
     });
 
-    const { isRegistered } = data;
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw error;
+    }
+  }
+};
 
-    return isRegistered;
+export const signup = async ({
+  userType,
+  userName,
+  userPhone,
+  userUniv,
+  userId,
+  userMajor,
+}: Partial<FormSchemaType>) => {
+  const isUnivStudnet = userType === "univ";
+  const baseBody = {
+    depositorName: userName,
+    phoneNumber: userPhone,
+  };
+  const univBody = {
+    ...baseBody,
+    university: userUniv,
+    studentMajor: userMajor,
+    studentCode: userId,
+  };
+
+  try {
+    const { data } = await instance.post(
+      "/users/register",
+      isUnivStudnet ? univBody : baseBody,
+    );
+
+    return data;
   } catch (error) {
     if (error instanceof AxiosError) {
       throw error;
