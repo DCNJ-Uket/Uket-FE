1e91b1961799dbdfcc3e9a1cd0c0c7873c0d756b
lunarmoon7 <zentechie7@gmail.com>
2024-04-30 13:57:36 +0900

    refactor: userType에 따라 Schema 구성 변경
    - Form 제출 시, 회원가입 mutation을 실행합니다.

--- src/pages/signup/_hooks/useStackForm.ts
+++ src/pages/signup/_hooks/useStackForm.ts
@@ -2,42 +2,65 @@ import { z } from "zod";
 import { UseFormReturn, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import { useMutationSignup } from "@/hooks/mutations/useMutationSignup";
+
 import { EXP } from "../_utils/vaildate";
 
-export interface ActivityParams {
-  form: UseFormReturn<z.infer<typeof FormSchema>, unknown, undefined>;
-}
+export type FormSchemaType = z.infer<typeof FormSchema>;
+export type FormType = UseFormReturn<FormSchemaType, unknown, undefined>;
 
-const FormSchema = z.object({
-  userType: z.enum(["univ", "no_univ"], {
-    required_error: "You need to select a notification type.",
-  }),
-  userName: z.string().regex(EXP.name),
-  userPhone: z.string().regex(EXP.phone),
-  userEmail: z.string().regex(EXP.email),
-  userId: z.string(),
-  userUniv: z.string(),
-  userMajor: z.string(),
-  userEmailAuth: z.string(),
-});
+export const FormSchema = z
+  .object({
+    userType: z.enum(["univ", "no_univ"], {
+      required_error: "You need to select a notification type.",
+    }),
+    userName: z.string().regex(EXP.name),
+    userPhone: z.string().regex(EXP.phone),
+    userEmail: z.string().regex(EXP.email).optional(),
+    userId: z.string().optional(),
+    userUniv: z.string().optional(),
+    userMajor: z.string().optional(),
+    userEmailAuth: z.string().optional(),
+  })
+  .refine(data => {
+    if (data.userType === "no_univ") return true;
+    else {
+      const { userEmail, userId, userUniv, userMajor, userEmailAuth } = data;
+      if (
+        userEmail === undefined ||
+        userId === undefined ||
+        userUniv === undefined ||
+        userMajor === undefined ||
+        userEmailAuth === undefined
+      )
+        return false;
+    }
+    return true;
+  });
 
 export const useStackForm = () => {
-  const form = useForm<z.infer<typeof FormSchema>>({
+  const { mutate } = useMutationSignup();
+
+  const form = useForm<FormSchemaType>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       userName: "",
       userPhone: "",
-      userEmail: "",
-      userId: "",
-      userUniv: "",
-      userMajor: "",
-      userEmailAuth: "",
     },
     mode: "onChange",
   });
 
-  const onSubmit = (data: z.infer<typeof FormSchema>) => {
-    console.log(data);
+  const onSubmit = (data: FormSchemaType) => {
+    const { userType, userName, userPhone, userUniv, userId, userMajor } = data;
+
+    mutate({
+      userType,
+      userName,
+      userPhone,
+      userUniv,
+      userId,
+      userMajor,
+    });
   };
 
   return { form, onSubmit };
