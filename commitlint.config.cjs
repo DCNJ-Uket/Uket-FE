56178d9ee078606b6d630035e43c8a1d50cf6c7a
lunarmoon7 <zentechie7@gmail.com>
2024-05-02 11:26:07 +0900

    chore: 주석 삭제

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,31 +1,3 @@
-// module.exports = {
-//     extends: ["@commitlint/config-conventional"],
-//     rules: {
-//       "header-max-length": [2, "always", 100],
-//       "subject-case": [0],
-//       "subject-empty": [2, "never"],
-//       "subject-full-stop": [2, "never", "."],
-//       "type-case": [2, "always", "lower-case"],
-//       "type-empty": [2, "never"],
-//       "type-enum": [
-//         2,
-//         "always",
-//         [
-//           "feat",
-//           "fix",
-//           "docs",
-//           "style",
-//           "refactor",
-//           "chore",
-//           "perf",
-//           "test",
-//           "ci",
-//           "build",
-//         ],
-//       ],
-//     },
-//   };
-
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
