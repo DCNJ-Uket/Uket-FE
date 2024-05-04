245643985fd77b97c459f6f1f444c3002d757f28
lunarmoon7 <zentechie7@gmail.com>
2024-05-04 20:03:38 +0900

    style: 버튼 스타일 변경

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -7,8 +7,8 @@ import { GOOGLE_LOGIN_URL, KAKAO_LOGIN_URL } from "@/constants/auth_url";
 const LoginPage = () => {
   return (
     <main className="flex flex-col justify-evenly items-center h-full">
-      <main className="flex flex-col justify-evenly w-full h-full">
-        <section className="container flex flex-col gap-4 w-full">
+      <main className="container flex flex-col justify-evenly w-full h-full">
+        <section className="flex flex-col gap-4 items-center w-full">
           <h1 className="text-2xl font-black">
             <p>학교 축제 티켓을</p>
             <p>예매해 볼까요?</p>
@@ -17,13 +17,13 @@ const LoginPage = () => {
             회원가입/로그인 방식을 선택해 주세요.
           </h2>
         </section>
-        <section className="container flex flex-col gap-2 justify-center items-center w-full">
-          <Link to={KAKAO_LOGIN_URL} className="block w-full">
+        <section className="flex flex-col gap-2 justify-center items-center w-full">
+          <Link to={KAKAO_LOGIN_URL} className="w-full sm:w-80">
             <Button className="h-12 w-full rounded-xl bg-[#FEE500] text-base text-[#191919] hover:bg-[#eed600]">
               카카오 계정으로 계속하기
             </Button>
           </Link>
-          <Link to={GOOGLE_LOGIN_URL} className="block w-full">
+          <Link to={GOOGLE_LOGIN_URL} className="w-full sm:w-80">
             <Button className="w-full h-12 text-base text-black bg-transparent rounded-xl border hover:bg-primary-foreground">
               구글 계정으로 계속하기
             </Button>
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -16,15 +16,15 @@ const MainPage = () => {
           </h1>
         </header>
         <section className="flex flex-col gap-2 justify-center items-center w-full">
-          <Link to="/select-univ" className="w-full">
+          <Link to="/select-univ" className="block w-full text-center">
             <Button className="p-6 w-full text-base text-white rounded-xl border border-brand bg-brand hover:bg-brand/80 sm:w-80">
               축제 예매하기
             </Button>
           </Link>
-          <Button className="p-6 w-full text-base bg-transparent rounded-xl border border-brand text-brand hover:bg-primary-foreground sm:w-80">
-            내 티켓 확인하기
-          </Button>
-        </section>
+                      <Button className="p-6 w-full text-base bg-transparent rounded-xl border border-brand text-brand hover:bg-primary-foreground sm:w-80">
+              내 티켓 확인하기
+            </Button>
+                  </section>
       </main>
     </main>
   );
