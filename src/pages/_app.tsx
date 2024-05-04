ffc1f67257d74e3dcffff5124707bae56089dcf6
lunarmoon7 <zentechie7@gmail.com>
2024-05-04 19:21:48 +0900

    style: Nav 컴포넌트 생성 및 추가
    - 전역으로 추가합니다.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 
+import Nav from "@/components/Nav";
+
 // TODO: 피그마 레이아웃 작업이 완료되면, 수정 또는 변경해주세요. 연결된 Route가 <Outlet />에 렌더링됩니다.
 const App = () => {
   return (
-    <section className="w-screen h-dvh">
-      <header>
-        <nav></nav>
+    <section className="flex relative flex-col w-screen h-dvh">
+      <header className="sticky top-0 left-0 z-10 bg-white">
+        <Nav />
       </header>
-      <main className="w-full h-full">
+      <main className="w-full grow">
         <Outlet />
       </main>
     </section>
