62c836cdfa7b5f9f35fc9e4a9d1165b9fea7c123
lunarmoon7 <zentechie7@gmail.com>
2024-05-04 23:28:57 +0900

    style: 임시 nav 렌더링 분기 처리
    - 배포 환경에서 루트 URL에 Nav가 있으면 적절하지 않은 레이아웃 스타일 처리

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import Nav from "@/components/Nav";
 
 // TODO: 피그마 레이아웃 작업이 완료되면, 수정 또는 변경해주세요. 연결된 Route가 <Outlet />에 렌더링됩니다.
 const App = () => {
+  const { pathname } = useLocation();
   return (
     <section className="flex relative flex-col w-screen h-dvh">
-      <header className="sticky top-0 left-0 z-10 bg-white">
-        <Nav />
-      </header>
+      {pathname !== "/" && (
+        <header className="sticky top-0 left-0 z-10 bg-white">
+          <Nav />
+        </header>
+      )}
       <main className="w-full grow">
         <Outlet />
       </main>
