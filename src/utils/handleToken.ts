bfea898a544e8e55a39188165c743e4ae442c192
lunarmoon7 <zentechie7@gmail.com>
2024-05-05 18:26:14 +0900

    refactor: 임시 로그인 유무 판별 로직 추가
    - 로컬 스토리지에 저장된 accessToken으로 로그인 유무를 판별합니다.

--- src/utils/handleToken.ts
+++ src/utils/handleToken.ts
@@ -2,3 +2,13 @@ export const saveTokenList = (accessToken: string, refreshToken: string) => {
   localStorage.setItem("accessToken", accessToken);
   localStorage.setItem("refreshToken", refreshToken);
 };
+
+export const getAccessToken = () => {
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    
+    return accessToken ? accessToken : null;
+  } catch (error) {
+    return null;
+  }
+};
