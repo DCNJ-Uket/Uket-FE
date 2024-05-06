45a80e20689ab6919304c482cb7b477d39c80b7f
Coggie (HwisikMoon) <zentechie7@gmail.com>
2024-05-06 16:06:25 +0900

    [style] 회원가입 페이지 레이아웃 수정 및 네비게이션 바 로그인 유무 표시 (#13)

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { Routes } from "@generouted/react-router";
 
 import "./index.css";
 import { ThemeProvider } from "./components/provider/ThemeProvider";
+import SVGProvider from "./components/provider/SVGProvider";
 import QueryProvider from "./components/provider/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
     <QueryProvider>
       <Routes />
+      <SVGProvider />
     </QueryProvider>
   </ThemeProvider>,
 );
--- src/pages/signup/_components/UserTypeItem.tsx
+++ src/pages/signup/_components/UserTypeItem.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import Icon from "@/components/Icon";
 
 import { cn } from "@/lib/utils";
 
@@ -6,20 +6,29 @@ interface UserTypeItemProps {
   title: string;
   desc: string;
   selected: boolean;
+  isUnivUser?: boolean;
 }
 
 const UserTypeItem = (props: UserTypeItemProps) => {
-  const { title, desc, selected } = props;
+  const { title, desc, selected, isUnivUser } = props;
+  const iconId = isUnivUser ? "univ_user" : "default_user";
 
   return (
-    <div
-      className={cn(
-        `flex flex-col justify-between gap-2 rounded-lg shadow-lg transition-colors duration-300 h-60 overflow-hidden`,
-        `${selected ? "bg-brand/50" : "bg-[#d7d7d7]"}`,
-      )}
-    >
-      <section className="flex gap-5 justify-evenly items-center">
-        <Skeleton className="w-20 h-20 rounded-full" />
+    <div className="flex overflow-hidden flex-col gap-2 justify-between h-60 rounded-lg shadow-lg">
+      <section
+        className={cn(
+          "flex aspect-square grow justify-center bg-[#f2f2f2] px-5 transition-colors duration-300",
+          selected ? "bg-brand/50" : "",
+        )}
+      >
+        <Icon
+          id={iconId}
+          size={110}
+          className={cn(
+            "self-end text-[#D9D9D9] transition-colors duration-300",
+            selected ? "text-brand" : "",
+          )}
+        />
       </section>
       <section className="flex flex-col gap-2 justify-center p-3 py-5 bg-white">
         <h1 className="font-bold">{title}</h1>
