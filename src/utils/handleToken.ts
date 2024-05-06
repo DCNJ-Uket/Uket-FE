a5a9e93b4af9ea0ec4919ad205e0e0b10c032b5c
lunarmoon7 <zentechie7@gmail.com>
2024-05-06 21:55:37 +0900

    feat: accessToken만 저장하는 로직 추가

--- src/utils/handleToken.ts
+++ src/utils/handleToken.ts
@@ -8,6 +8,10 @@ export const saveTokenList = (accessToken: string, refreshToken: string) => {
   });
 };
 
+export const setAccessToken = (accessToken: string) => {
+  localStorage.setItem("accessToken", accessToken);
+};
+
 export const getAccessToken = () => {
   try {
     const accessToken = localStorage.getItem("accessToken");
