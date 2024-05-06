aeff52619680c27d8eef6f73add1a651ce7f34ef
lunarmoon7 <zentechie7@gmail.com>
2024-05-06 16:05:15 +0900

    feat: react-cookie를 사용하기 위한 CookieProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import ReactDOM from "react-dom/client";
 import { Routes } from "@generouted/react-router";
 
 import "./index.css";
+
 import { ThemeProvider } from "./components/provider/ThemeProvider";
 import QueryProvider from "./components/provider/QueryProvider";
+import CookieProvider from "./components/provider/CookieProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
     <QueryProvider>
-      <Routes />
+      <CookieProvider>
+        <Routes />
+      </CookieProvider>
     </QueryProvider>
   </ThemeProvider>,
 );
