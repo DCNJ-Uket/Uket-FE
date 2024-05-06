13d85453b8f2e3484569676d49cdec75e5c0eb74
lunarmoon7 <zentechie7@gmail.com>
2024-05-06 21:56:00 +0900

    feat: Routes에 Suspense 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import { Suspense } from "react";
 import { Routes } from "@generouted/react-router";
 
 import "./index.css";
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
     <QueryProvider>
       <CookieProvider>
-        <Routes />
+        <Suspense fallback={null}>
+          <Routes />
+        </Suspense>
         <SVGProvider />
       </CookieProvider>
     </QueryProvider>
