fd5b07b29df3be13fd769c9d160ce66a5c44f5e3
SonSuBin129 <ttokky0129@gmail.com>
2024-05-08 01:49:49 +0900

    style: 티켓 확인 페이지 슬라이딩 애니메이션 구현

--- src/pages/ticket-list/index.tsx
+++ src/pages/ticket-list/index.tsx
@@ -3,10 +3,42 @@ import { useState } from "react";
 import TicketItem from "./_components/TicketItem";
 
 const TicketList = () => {
-  const [selectedTicket, setSelectedTicket] = useState<number | null>(0);
+  const [selectedTicket, setSelectedTicket] = useState<number>(0);
+  const [touchStartX, setTouchStartX] = useState<number>(0);
+  const [slidePosition, setSlidePosition] = useState<number>(0);
+
+  const ticketWidth = 287; //TicketItem의 너비
+  const gapWidth = 10; //TicketItem 사이 gap
+  const slideWidth = ticketWidth + gapWidth;
+  const ticketCount = 14; // 티켓 아이템의 개수
+
+  const onTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    setTouchStartX(event.changedTouches[0].pageX);
+  };
+
+  const onTouchEnd = (event: React.TouchEvent<HTMLDivElement>) => {
+    const touchEndX = event.changedTouches[0].pageX;
+    const distanceX = touchStartX - touchEndX;
+
+    const maxSlidePosition = (ticketCount - 1) * slideWidth;
+
+    let newPosition =
+      distanceX < 0 ? slidePosition - slideWidth : slidePosition + slideWidth;
+
+    // 슬라이드 이동 범위 제한
+    if (newPosition < 0) {
+      newPosition = 0;
+    } else if (newPosition > maxSlidePosition) {
+      newPosition = maxSlidePosition;
+    }
+
+    setSelectedTicket(Math.round(newPosition / slideWidth));
+    setSlidePosition(newPosition);
+  };
 
   const handleSelectTicket = (index: number) => {
     setSelectedTicket(index);
+    setSlidePosition(index * slideWidth);
   };
 
   return (
@@ -15,14 +47,25 @@ const TicketList = () => {
         <header className="text-xl font-black">
           <p>내 티켓</p>
         </header>
-        <section className="flex h-full flex-row items-center gap-[10px] overflow-hidden">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <TicketItem
-              key={index}
-              selected={selectedTicket === index}
-              onSelect={() => handleSelectTicket(index)}
-            />
-          ))}
+        <section className="h-full overflow-hidden">
+          <div
+            className="flex h-full flex-row items-center"
+            onTouchStart={onTouchStart}
+            onTouchEnd={onTouchEnd}
+            style={{
+              transition: "transform 0.4s ease-in-out",
+              transform: `translateX(-${slidePosition}px)`,
+              gap: gapWidth,
+            }}
+          >
+            {Array.from({ length: ticketCount }).map((_, index) => (
+              <TicketItem
+                key={index}
+                selected={selectedTicket === index}
+                onSelect={() => handleSelectTicket(index)}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </main>
