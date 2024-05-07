9e1d6a46d8379a3572ed24e39dcde16c068705a4
SonSuBin129 <ttokky0129@gmail.com>
2024-05-07 22:44:33 +0900

    style: ticket 스타일 수정

--- src/pages/ticket-list/_components/TicketItem.tsx
+++ src/pages/ticket-list/_components/TicketItem.tsx
@@ -1,16 +1,14 @@
-
-
 const TicketItem = () => {
   return (
-    <div className="grid h-full w-full min-w-max cursor-pointer grid-cols-1 grid-rows-4 items-center justify-center divide-y divide-dashed">
-      <div className="row-span-3 flex h-full flex-col gap-3.5 rounded-b-3xl  rounded-t-lg bg-white p-4 shadow-lg">
-        <div className="h-full  min-h-[259px] w-full rounded-lg bg-green-800"></div>
-        <div className="flex w-full flex-col gap-3 p-[3px]">
+    <div className="grid max-h-[500px] w-full min-w-[300px] cursor-pointer grid-cols-1 grid-rows-5 items-center justify-center divide-y divide-dashed">
+      <div className="row-span-4 grid h-full grid-flow-col grid-rows-2 gap-3.5 rounded-b-3xl  rounded-t-lg bg-white p-4 shadow-lg">
+        <div className="h-full w-full rounded-lg bg-green-800"></div>
+        <div className="flex w-full flex-col justify-around p-[3px]">
           <div>
-            <p className="text-[10px] text-[#5E5E6E]">건국대</p>
+            <p className="text-[12px] text-[#5E5E6E]">건국대</p>
             <p className="text-xl font-black">녹색지대 Day 1</p>
           </div>
-          <div className="grid max-h-max w-full grid-cols-2 grid-rows-2 gap-1.5 text-[10px]">
+          <div className="grid max-h-max w-full grid-cols-2 grid-rows-2 gap-1.5 text-[12px]">
             <div>
               <p className="font-bold">예매자</p>
               <p className="text-[#5E5E6E]">김건국</p>
@@ -35,7 +33,7 @@ const TicketItem = () => {
           102389751(일련번호)
         </div>
         {/* 추후에 qr코드 추가 */}
-        <div className="aspect-square h-full self-center bg-black"></div>
+        <div className="aspect-square h-full min-h-[60px] self-center bg-black"></div>
       </div>
     </div>
   );
