4da1daf30b4ddcb90d59a317cf282f8e4e7cc0c4
SonSuBin129 <ttokky0129@gmail.com>
2024-05-09 04:56:53 +0900

    style: 흰색 로고 추가

--- src/components/WhiteLogo.tsx
+++ src/components/WhiteLogo.tsx
@@ -1,12 +1,33 @@
+// import { Link } from "@/router";
+
+// import LogoImage from "/whiteLogo.png";
+
+// //TODO: 추후에 Logo.tsx와 합치기
+// const WhiteLogo = () => {
+//   return (
+//     <Link to="/main" className="pl-6 pt-2">
+//       <img src={LogoImage} width={62} loading="lazy" alt="로고" />
+//     </Link>
+//   );
+// };
+
+// export default WhiteLogo;
+
 import { Link } from "@/router";
 
-import LogoImage from "/whiteLogo.png";
+import WhiteLogoImg from "/whiteLogo.png";
 
-//TODO: 추후에 Logo.tsx와 합치기
-const WhiteLogo = () => {
+import { cn } from "@/lib/utils";
+
+interface LogoProps {
+  onActivity?: boolean;
+}
+
+const WhiteLogo = (props: LogoProps) => {
+  const { onActivity } = props;
   return (
-    <Link to="/main" className="pl-6 pt-2">
-      <img src={LogoImage} width={62} loading="lazy" alt="로고" />
+    <Link to="/main" className={cn(onActivity ? "pl-6 pt-2" : "")}>
+      <img src={WhiteLogoImg} width={62} loading="lazy" alt="로고" />
     </Link>
   );
 };
