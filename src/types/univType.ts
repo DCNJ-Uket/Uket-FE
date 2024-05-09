9ffebbb80d12c39aef4f7238b16a88f29ded64a5
lunarmoon7 <zentechie7@gmail.com>
2024-05-09 14:15:21 +0900

    feat: 대학 조회 및 학과 조회 API 응답 타입 추가

--- src/types/univType.ts
+++ src/types/univType.ts
@@ -1,9 +1,37 @@
-export type UniversityInfo = {
+export type FestivalUniversity = {
   id: number;
   name: string;
   logoUrl: string;
 };
 
-export type UniversityListResponse = {
-  items: UniversityInfo[];
+export type FestivalUniversityResponse = {
+  items: FestivalUniversity[];
+};
+
+export type UniversityInfo = {
+  campusName: string;
+  collegeinfourl: string;
+  schoolType: string;
+  link: string;
+  schoolGubun: string;
+  adres: string;
+  schoolName: string;
+  region: string;
+  totalCount: string;
+  estType: string;
+  seq: string;
+};
+
+export type MajorInfo = {
+  lClass: string;
+  facilName: string;
+  majorSeq: string;
+  mClass: string;
+  totalCount: string;
+};
+
+export type UnivOrMajorResponse<T> = {
+  dataSearch: {
+    content: T[];
+  };
 };
