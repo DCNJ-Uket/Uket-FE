3acd634e254fcc89dda4d2dd7fd203b4f23e1323
SonSuBin129 <ttokky0129@gmail.com>
2024-05-11 21:35:23 +0900

    feat: /buy-ticket 라우트 추가

---
+++ src/pages/buy-ticket/index.tsx
@@ -0,0 +1,9 @@
+const BuyTicket = () => {
+  return (
+    <main className="relative flex h-full flex-col items-center justify-evenly bg-[#F2F2F2]">
+      <main className="container mb-10 mt-6 flex h-full w-full flex-col gap-4"></main>
+    </main>
+  );
+};
+
+export default BuyTicket;
