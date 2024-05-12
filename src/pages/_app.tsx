2a87eb30221f2a7b49de6702b1bcaaa0e2aee1c4
lunarmoon7 <zentechie7@gmail.com>
2024-05-12 15:42:57 +0900

    refactor: Suspense 제거 및 CriticalErrorBoundary 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
 import { Outlet, useLocation } from "react-router-dom";
 
 import Nav from "@/components/Nav";
+import CriticalErrorBoundary from "@/components/CriticalErrorBoundary";
 
 // TODO: 피그마 레이아웃 작업이 완료되면, 수정 또는 변경해주세요. 연결된 Route가 <Outlet />에 렌더링됩니다.
 const App = () => {
   const { pathname } = useLocation();
   return (
     <section className="flex relative flex-col w-screen h-dvh">
-      {pathname !== "/" && (
-        <header className="sticky top-0 left-0 z-10 bg-white">
-          <Nav />
-        </header>
-      )}
-      <main className="w-full grow">
-        <Outlet />
-      </main>
+      <CriticalErrorBoundary>
+        {pathname !== "/" && (
+          <header className="sticky top-0 left-0 z-10 bg-white">
+            <Nav />
+          </header>
+        )}
+        <main className="w-full grow">
+          <Outlet />
+        </main>
+      </CriticalErrorBoundary>
     </section>
   );
 };
