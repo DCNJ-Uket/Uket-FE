34e9dce548fc29534d441332662d4f74d8b69807
SonSuBin129 <ttokky0129@gmail.com>
2024-05-13 19:04:16 +0900

    feat: 티켓구매페이지 stackflow 생성

---
+++ src/utils/buyTicketFlow.ts
@@ -0,0 +1,27 @@
+import { stackflow } from "@stackflow/react";
+import { basicRendererPlugin } from "@stackflow/plugin-renderer-basic";
+import { basicUIPlugin } from "@stackflow/plugin-basic-ui";
+
+import TimeActivity from "@/pages/buy-ticket/_components/TimeActivity";
+import DateActivity from "@/pages/buy-ticket/_components/DateActivity";
+import CompleteActivity from "@/pages/buy-ticket/_components/CompleteActivity";
+
+import "@stackflow/plugin-basic-ui/index.css";
+
+export const { Stack, useFlow, activities } = stackflow({
+  transitionDuration: 500,
+  plugins: [
+    basicRendererPlugin(),
+    basicUIPlugin({
+      theme: "cupertino",
+    }),
+  ],
+  activities: {
+    DateActivity,
+    TimeActivity,
+    CompleteActivity,
+  },
+  initialActivity: () => "DateActivity",
+});
+
+export type TypeActivities = typeof activities;
