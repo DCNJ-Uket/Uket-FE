089a80bd5883ff91d5d2ffe5c54f4d618364208e
SonSuBin129 <ttokky0129@gmail.com>
2024-05-13 19:26:43 +0900

    style: 티켓 예매 페이지 수정

--- src/pages/buy-ticket/index.tsx
+++ src/pages/buy-ticket/index.tsx
@@ -1,15 +1,13 @@
-import TimeItem from "./_components/TimeItem";
+import { Suspense } from "react";
+
+import { Stack } from "@/utils/buyTicketFlow";
 
 const BuyTicket = () => {
   return (
-    <main className="relative flex h-full flex-col items-center justify-evenly bg-[#F2F2F2]">
-      <main className="container mb-10 mt-6 flex h-full w-full flex-col gap-4">
-        <section className="flex h-full w-full flex-col gap-4 overflow-y-scroll">
-          {Array.from({ length: 10 }).map((_, index) => (
-            <TimeItem key={index} title={`Day ${index + 1}`} />
-          ))}
-        </section>
-      </main>
+    <main>
+      <Suspense>
+        <Stack />
+      </Suspense>
     </main>
   );
 };
