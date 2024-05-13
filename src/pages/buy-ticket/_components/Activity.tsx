648712780e2a22ed083ae5b0a4bf62566f3aa891
SonSuBin129 <ttokky0129@gmail.com>
2024-05-13 19:24:38 +0900

    style: Activity 추가

--- src/pages/buy-ticket/_components/Activity.tsx
+++ src/pages/buy-ticket/_components/Activity.tsx
@@ -24,7 +24,7 @@ const ActivityContent = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
   return (
-    <main className="flex h-full flex-col items-center overflow-y-scroll bg-[#F2F2F2] px-[22px] pb-6 pt-8">
+    <main className="flex h-full flex-col items-center overflow-y-scroll bg-[#F2F2F2] px-[22px] pt-8">
       <section
         className={cn(
           "flex w-full grow flex-col justify-center gap-4",
@@ -43,7 +43,9 @@ const ActivityFooter = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
-  return <footer className={cn("sticky bottom-0", className)} {...props} />;
+  return (
+    <footer className={cn("sticky bottom-0 pt-6", className)} {...props} />
+  );
 };
 ActivityFooter.displayName = "ActivityFooter";
 
