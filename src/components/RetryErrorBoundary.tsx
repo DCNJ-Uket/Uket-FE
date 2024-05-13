9b046f8dbe6fb3dd587f24b32063465d0364c5c4
lunarmoon7 <zentechie7@gmail.com>
2024-05-13 12:06:43 +0900

    refactor: ErrorBoundary resetKeys 설정

--- src/components/RetryErrorBoundary.tsx
+++ src/components/RetryErrorBoundary.tsx
@@ -4,12 +4,19 @@ import { useQueryErrorResetBoundary } from "@tanstack/react-query";
 
 import { Button } from "./ui/button";
 
-const RetryErrorBoundary = ({ children }: { children: React.ReactNode }) => {
+const RetryErrorBoundary = ({
+  children,
+  resetKeys,
+}: {
+  children: React.ReactNode;
+  resetKeys?: unknown[];
+}) => {
   const { reset } = useQueryErrorResetBoundary();
 
   return (
     <ErrorBoundary
       onReset={reset}
+      resetKeys={resetKeys}
       onError={error => {
         if (isAxiosError(error) && error?.response?.status === 500) {
           throw error;
@@ -19,14 +26,14 @@ const RetryErrorBoundary = ({ children }: { children: React.ReactNode }) => {
         const errorMessage = error.response.data.message;
 
         return (
-          <div className="flex flex-col gap-10 justify-center items-center w-full h-full">
+          <div className="flex h-full w-full flex-col items-center justify-center gap-10">
             <header className="text-center">
               <h1 className="text-2xl font-bold">잠시 후 다시 시도해주세요</h1>
               <h2 className="text-gray-700">{errorMessage}</h2>
             </header>
             <Button
               onClick={() => resetErrorBoundary()}
-              className="py-5 w-full max-w-sm text-base bg-brand"
+              className="w-full max-w-sm bg-brand py-5 text-base"
             >
               다시 시도
             </Button>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -21,18 +21,18 @@ const HomePage = () => {
 
   return (
     <main className="relative flex h-full flex-col items-center bg-[#F2F2F2]">
-      <main className="container flex flex-col gap-3 mt-2 w-full h-full bg-white">
+      <main className="container mt-2 flex h-full w-full flex-col gap-3 bg-white">
         <UnivSelector currentUniv={univName} onSelect={handleSelectUnivItem} />
-        <RetryErrorBoundary>
+        <RetryErrorBoundary resetKeys={[univId]}>
           <Suspense fallback={<FestivalSectionFallback />}>
             <FestivalSection univId={univId} />
           </Suspense>
         </RetryErrorBoundary>
-        <footer className="flex gap-3 justify-center items-center mb-3 w-full bg-white">
-          <Button className="bg-white rounded-lg border basis-1/2 border-brand text-brand hover:bg-primary-foreground sm:w-80">
+        <footer className="mb-3 flex w-full items-center justify-center gap-3 bg-white">
+          <Button className="basis-1/2 rounded-lg border border-brand bg-white text-brand hover:bg-primary-foreground sm:w-80">
             내 티켓 확인
           </Button>
-          <Button className="rounded-lg border basis-1/2 border-brand bg-brand hover:bg-brand/80 sm:w-80">
+          <Button className="basis-1/2 rounded-lg border border-brand bg-brand hover:bg-brand/80 sm:w-80">
             예매하기
           </Button>
         </footer>
