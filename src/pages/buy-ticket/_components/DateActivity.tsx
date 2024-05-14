25a20ad87e1f87d5409fc63da78aa8e45e55f210
SonSuBin129 <ttokky0129@gmail.com>
2024-05-15 02:12:48 +0900

    refactor: HeaderItem 통해서 DateActivity와 TimeActivity 추상화 적용

--- src/pages/buy-ticket/_components/DateActivity.tsx
+++ src/pages/buy-ticket/_components/DateActivity.tsx
@@ -2,6 +2,7 @@ import { ActivityComponentType } from "@stackflow/react";
 import { AppScreen } from "@stackflow/plugin-basic-ui";
 
 import NextButton from "./NextButton";
+import HeaderItem from "./HeaderItem";
 import DateItem from "./DateItem";
 import {
   Activity,
@@ -10,17 +11,13 @@ import {
   ActivityHeader,
 } from "./Activity";
 
-
 const DateActivity: ActivityComponentType = () => {
   return (
     <AppScreen appBar={{ border: false, height: "56px" }}>
       <Activity>
         <ActivityContent>
           <ActivityHeader>
-            <p className="text-[15px] font-light text-[#5E5E6E]">STEP 01</p>
-            <h1 className="text-[23px] font-black">
-              <p>예매 날짜를 선택해주세요.</p>
-            </h1>
+            <HeaderItem step={"01"} content={"예매 날짜를 선택해 주세요."} />
           </ActivityHeader>
           {Array.from({ length: 14 }).map((_, index) => (
             <DateItem key={index} title={`Day ${index + 1}`} />
--- src/pages/buy-ticket/_components/TimeActivity.tsx
+++ src/pages/buy-ticket/_components/TimeActivity.tsx
@@ -1,9 +1,9 @@
 import { ActivityComponentType } from "@stackflow/react";
 import { AppScreen } from "@stackflow/plugin-basic-ui";
 
-
 import TimeItem from "./TimeItem";
 import NextButton from "./NextButton";
+import HeaderItem from "./HeaderItem";
 import {
   Activity,
   ActivityContent,
@@ -17,10 +17,7 @@ const TimeActivity: ActivityComponentType = () => {
       <Activity>
         <ActivityContent>
           <ActivityHeader>
-            <p className="text-[15px] font-light text-[#5E5E6E]">STEP 02</p>
-            <h1 className="text-[23px] font-black">
-              <p>예매 시간을 선택해주세요.</p>
-            </h1>
+            <HeaderItem step={"02"} content={"예매 시간을 선택해 주세요."} />
           </ActivityHeader>
           {Array.from({ length: 14 }).map((_, index) => (
             <TimeItem key={index} title={`Time ${index + 1}`} />
