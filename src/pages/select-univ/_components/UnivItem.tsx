6c7b7fc3e3a3c59aeef35f67d23fbc24e40c307a
lunarmoon7 <zentechie7@gmail.com>
2024-05-15 14:35:41 +0900

    style: 대학 선택 페이지의 대학 목록과 아이템 스타일 수정

--- src/pages/select-univ/_components/UnivItem.tsx
+++ src/pages/select-univ/_components/UnivItem.tsx
@@ -5,12 +5,12 @@ import { FestivalUniversity } from "@/types/univType";
 import { cn } from "@/lib/utils";
 
 interface UnivItemProps extends Pick<FestivalUniversity, "logoUrl" | "name"> {
-  selected?: boolean;
+  isSelected?: boolean;
   onSelect: () => void;
 }
 
 const UnivItem = (props: UnivItemProps) => {
-  const { selected, onSelect, name, logoUrl } = props;
+  const { isSelected, onSelect, name, logoUrl } = props;
   const univLogo = logoUrl ? (
     <img
       src={logoUrl}
@@ -20,18 +20,18 @@ const UnivItem = (props: UnivItemProps) => {
       className="object-cover"
     />
   ) : (
-    <Skeleton className="w-full h-full rounded-full" />
+    <Skeleton className="h-full w-full rounded-full" />
   );
 
   return (
     <div
-      className="flex flex-col gap-3 justify-center items-center cursor-pointer"
+      className="flex cursor-pointer flex-col items-center justify-center gap-3"
       onClick={onSelect}
     >
       <div
         className={cn(
           "aspect-square h-full w-full rounded-2xl bg-white p-3 shadow-md transition-colors duration-300",
-          selected && "bg-brand/50",
+          isSelected && "bg-brand/50",
         )}
       >
         {univLogo}
