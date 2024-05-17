de426a1011fd4e5028d4c9e7b042dd3194b180c1
SonSuBin129 <ttokky0129@gmail.com>
2024-05-17 16:24:22 +0900

    refactor: WhiteLogo route 경로 수정

--- src/components/WhiteLogo.tsx
+++ src/components/WhiteLogo.tsx
@@ -1,18 +1,3 @@
-// import { Link } from "@/router";
-
-// import LogoImage from "/whiteLogo.png";
-
-// //TODO: 추후에 Logo.tsx와 합치기
-// const WhiteLogo = () => {
-//   return (
-//     <Link to="/main" className="pl-6 pt-2">
-//       <img src={LogoImage} width={62} loading="lazy" alt="로고" />
-//     </Link>
-//   );
-// };
-
-// export default WhiteLogo;
-
 import { Link } from "@/router";
 
 import WhiteLogoImg from "/whiteLogo.png";
@@ -26,7 +11,7 @@ interface LogoProps {
 const WhiteLogo = (props: LogoProps) => {
   const { onActivity } = props;
   return (
-    <Link to="/main" className={cn(onActivity ? "pl-6 pt-2" : "")}>
+    <Link to="/" className={cn(onActivity ? "pl-6 pt-2" : "")}>
       <img src={WhiteLogoImg} width={62} loading="lazy" alt="로고" />
     </Link>
   );
