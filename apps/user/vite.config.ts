2714085a8674dc8ca1ba854ad5a48ceb1109a61c
lunarmoon7 <zentechie7@gmail.com>
2024-05-20 23:26:33 +0900

    chore: vite.config.ts plugin 변경

--- apps/user/vite.config.ts
+++ apps/user/vite.config.ts
@@ -2,30 +2,18 @@ import path from "path";
 
 import mkcert from "vite-plugin-mkcert";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import generouted from "@generouted/react-router/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), generouted(), mkcert()],
+  plugins: [react({
+    jsxRuntime: 'classic',
+  }), generouted(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@ui": path.resolve(__dirname, "../../packages/ui/src"),
-    },
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      onwarn(warning, defaultHandler) {
-        if (warning.code === "SOURCEMAP_ERROR") {
-          return
-        }
-
-        defaultHandler(warning)
-      },
-      // External packages that should not be bundled into your library.
-      external: ["react", "react-dom", "react-router-dom", "react/jsx-runtime"],
     },
   },
 });
