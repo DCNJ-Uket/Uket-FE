4eb1b73b52457e24567bd1e0f78c83350514cca4
lunarmoon7 <zentechie7@gmail.com>
2024-05-23 13:03:04 +0900

    refactor: useQueryUserInfo 호출 위치 변경
    - 기존 Nav에서 Profile로 컴포넌트 책임을 분리했습니다.

--- apps/user/src/components/Nav.tsx
+++ apps/user/src/components/Nav.tsx
@@ -1,44 +1,27 @@
 import { useLocation } from "react-router-dom";
-import { cn } from "@uket/ui/lib/utils";
 import { Button } from "@uket/ui/components/ui/button";
 
-import { useQueryUserInfo } from "@/hooks/queries/useQueryUserInfo";
-
+import RetryErrorBoundary from "./RetryErrorBoundary";
 import Profile from "./Profile";
 import Logo from "./Logo";
 
-import { Link, Path } from "@/router";
+import { Path } from "@/router";
 
 const PRIVATE: Path[] = ["/login", "/signup"];
 
 const Nav = () => {
   const { pathname } = useLocation();
-  const { data: userInfo } = useQueryUserInfo();
-  const isPrivatePath = PRIVATE.includes(pathname as Path);
-  const ladingPageTextColor = pathname === "/" && "text-white";
 
-  const profileComponent = userInfo ? (
-    <Button
-      variant="link"
-      className={cn("p-0 pt-1 font-bold", ladingPageTextColor)}
-    >
-      <Profile name={userInfo.name} profileImage={userInfo.profileImage} />
-    </Button>
-  ) : (
-    <Link to="/login">
-      <Button
-        variant="link"
-        className={cn("p-0 pt-1 font-bold", ladingPageTextColor)}
-      >
-        로그인
-      </Button>
-    </Link>
-  );
+  const isPrivatePath = PRIVATE.includes(pathname as Path);
 
   return (
     <nav className="container my-2 flex h-10 w-full items-center justify-between self-stretch">
       <Logo />
-      {!isPrivatePath && profileComponent}
+      {!isPrivatePath && (
+        <RetryErrorBoundary fallbackComponent={<Button>다시 시도</Button>}>
+          <Profile />
+        </RetryErrorBoundary>
+      )}
     </nav>
   );
 };
--- apps/user/src/components/Profile.tsx
+++ apps/user/src/components/Profile.tsx
@@ -1,26 +1,49 @@
-import { UserInfoResponse } from "@/types/userType";
+import { Link, useLocation } from "react-router-dom";
+import { cn } from "@uket/ui/lib/utils";
+import { Button } from "@uket/ui/components/ui/button";
 
-interface ProfileProps
-  extends Pick<UserInfoResponse, "name" | "profileImage"> {}
+import { useQueryUserInfo } from "@/hooks/queries/useQueryUserInfo";
+
+import { reissue } from "@/api/auth";
 
 // TODO: 추후 디자인에 맞춰서 Drawer 또는 페이지로 변경
-const Profile = (props: ProfileProps) => {
-  const { profileImage, name } = props;
+const Profile = () => {
+  const { data: userInfo } = useQueryUserInfo();
+  const { pathname } = useLocation();
+  const ladingPageTextColor = pathname === "/" && "text-white";
 
   return (
     <>
-      {profileImage && name && (
-        <div className="flex items-center gap-3">
-          <div className="relative h-6 w-6">
-            <img
-              src={profileImage}
-              alt="프로필 이미지"
-              width={100}
-              className="h-full w-full rounded-full object-cover"
-            />
+      {userInfo && (
+        <Button
+          variant="link"
+          className={cn("p-0 pt-1 font-bold", ladingPageTextColor)}
+          onClick={async () => {
+            await reissue();
+          }}
+        >
+          <div className="flex items-center gap-3">
+            <div className="relative h-6 w-6">
+              <img
+                src={userInfo.profileImage}
+                alt="프로필 이미지"
+                width={100}
+                className="h-full w-full rounded-full object-cover"
+              />
+            </div>
+            <p className="font-bold">{userInfo.name}</p>
           </div>
-          <p className="font-bold">{name}</p>
-        </div>
+        </Button>
+      )}
+      {!userInfo && (
+        <Link to="/login">
+          <Button
+            variant="link"
+            className={cn("p-0 pt-1 font-bold", ladingPageTextColor)}
+          >
+            로그인
+          </Button>
+        </Link>
       )}
     </>
   );
