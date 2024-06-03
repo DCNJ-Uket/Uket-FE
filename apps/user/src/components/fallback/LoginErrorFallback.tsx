60f3399ca76100fc313e40683fef89601ac4bec5
lunarmoon7 <zentechie7@gmail.com>
2024-06-03 20:24:45 +0900

    feat: reissue 실패 시 로그인 페이지로 유도하는 기능 추가

--- apps/user/src/components/fallback/LoginErrorFallback.tsx
+++ apps/user/src/components/fallback/LoginErrorFallback.tsx
@@ -2,18 +2,24 @@ import { FallbackProps } from "react-error-boundary";
 import { cn } from "@uket/ui/lib/utils";
 import { Button } from "@uket/ui/components/ui/button";
 
+import { useNavigate } from "@/router";
+
 interface LoginErrorFallbackProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     FallbackProps {}
 
 const LoginErrorFallback = (props: LoginErrorFallbackProps) => {
   const { className, resetErrorBoundary, ...rest } = props;
+  const navigate = useNavigate();
 
   return (
     <Button
       variant="link"
       className={cn("p-0 text-xs underline", className)}
-      onClick={resetErrorBoundary}
+      onClick={() => {
+        resetErrorBoundary();
+        navigate("/login");
+      }}
       {...rest}
     >
       다시 시도
