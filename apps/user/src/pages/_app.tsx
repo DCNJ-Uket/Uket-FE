d62ebdc4682c82a52a077a3269cd1724670c0e6c
lunarmoon7 <zentechie7@gmail.com>
2024-06-07 12:36:15 +0900

    feat: Redirects 추가
    - 로그인을 하지 않은 경우, 로그인 페이지로 이동하는 로직을 추가했습니다.

--- apps/user/src/pages/_app.tsx
+++ apps/user/src/pages/_app.tsx
@@ -3,21 +3,26 @@ import { Outlet, useLocation } from "react-router-dom";
 import Nav from "@/components/Nav";
 import CriticalErrorBoundary from "@/components/error/CriticalErrorBoundary";
 
+import Redirects from "@/utils/redirects";
+
 const App = () => {
   const { pathname } = useLocation();
+
   return (
-    <section className="relative flex h-dvh w-screen flex-col">
-      <CriticalErrorBoundary>
-        {!["/", "/ticket-detail"].includes(pathname) && (
+    <CriticalErrorBoundary>
+      <section className="relative flex h-dvh w-screen flex-col">
+        {!["/"].includes(pathname) && (
           <header className="sticky left-0 top-0 z-10 bg-white">
             <Nav />
           </header>
         )}
         <main className="w-full grow">
-          <Outlet />
+          <Redirects>
+            <Outlet />
+          </Redirects>
         </main>
-      </CriticalErrorBoundary>
-    </section>
+      </section>
+    </CriticalErrorBoundary>
   );
 };
 
