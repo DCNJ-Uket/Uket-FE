4e3441714ec95ce4477c557f85327972f7de058d
lunarmoon7 <zentechie7@gmail.com>
2024-06-14 09:31:30 +0900

    style: TicketListSection 스타일 수정

--- apps/user/src/pages/ticket-list/_components/TicketListSection.tsx
+++ apps/user/src/pages/ticket-list/_components/TicketListSection.tsx
@@ -20,13 +20,14 @@ const TicketListSection = () => {
         className={cn(
           "-ml-1",
           myTicketList.length === 1 && "flex justify-center",
+          myTicketList.length >= 2 && "mx-3 sm:ml-0"
         )}
       >
         {myTicketList.length > 0 &&
           myTicketList.map(ticket => (
             <CarouselItem
               key={ticket.ticketId}
-              className="basis-5/6 pb-2 pl-2 sm:basis-1/2 sm:pl-10 md:basis-1/2 lg:basis-1/3 xl:basis-1/3 2xl:basis-1/3"
+              className="basis-11/12 pb-2 pl-2 sm:basis-1/2 sm:pl-10 md:basis-1/2 lg:basis-1/3 xl:basis-1/3 2xl:basis-1/3"
             >
               <div className="p-1">
                 <Ticket ticket={ticket} qrCode={data[ticket.ticketId]} />
