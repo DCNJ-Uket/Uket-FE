2f8fe3489e2e51464a83bf70cf7cb3f4e33741ba
SonSuBin129 <ttokky0129@gmail.com>
2024-06-23 00:20:24 +0900

    feat: 회원정보 조회에서 내 티켓 확인하기로 route 추가

--- apps/user/src/pages/myinfo/index.tsx
+++ apps/user/src/pages/myinfo/index.tsx
@@ -1,9 +1,55 @@
+import { Button } from "@uket/ui/components/ui/button";
+
+import { useQueryUserInfo } from "@/hooks/queries/useQueryUserInfo";
+
+import { useNavigate } from "@/router";
+
 const MyInfo = () => {
+  const { data: userInfo } = useQueryUserInfo();
+  const navigate = useNavigate();
+
   return (
     <main className="relative flex h-full flex-col items-center justify-evenly bg-[#F2F2F2]">
-      <main>
-        <header>김건국</header>
+      <main className="container flex h-full w-full flex-col justify-evenly">
+        <div className="flex w-full flex-col items-start gap-7 rounded-xl bg-white p-7">
+          {userInfo && (
+            <div className="flex items-center gap-6">
+              <div className="relative h-11 w-11">
+                <img
+                  src={userInfo.profileImage}
+                  alt="프로필 이미지"
+                  width={100}
+                  className="h-full w-full rounded-full object-cover"
+                />
+              </div>
+              <p className="text-xl font-bold">{userInfo.name}</p>
+            </div>
+          )}
+          <Button
+            variant="outline"
+            className="w-full border-2 border-black py-2 text-center text-sm font-semibold"
+            onClick={() => {
+              navigate("/ticket-list");
+            }}
+          >
+            내 티켓 확인하기
+          </Button>
+        </div>
         <section></section>
+        <div className="flex flex-col gap-4">
+          <Button
+            variant="secondary"
+            className="w-full rounded-xl bg-white p-6 text-sm font-medium text-black hover:bg-slate-200 sm:w-80"
+          >
+            로그아웃
+          </Button>
+          <Button
+            variant="secondary"
+            className="w-full rounded-xl bg-white p-6 text-sm font-medium text-black hover:bg-slate-200 sm:w-80"
+          >
+            회원탈퇴
+          </Button>
+        </div>
       </main>
     </main>
   );
