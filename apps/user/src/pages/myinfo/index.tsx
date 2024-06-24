2289940c3c7824b3f8dac0e7a9c3cb16cab32a5f
SonSuBin129 <ttokky0129@gmail.com>
2024-06-24 13:16:43 +0900

    style: 스위치 버튼 추가

--- apps/user/src/pages/myinfo/index.tsx
+++ apps/user/src/pages/myinfo/index.tsx
@@ -1,13 +1,13 @@
+import { Switch } from "@uket/ui/components/ui/switch";
 import { Button } from "@uket/ui/components/ui/button";
 
 import { useQueryUserInfo } from "@/hooks/queries/useQueryUserInfo";
 
 import InfoItem from "./_components/InfoItem";
 
-
 import { useNavigate } from "@/router";
 
-//TODO: 유저정보 userInfo로 채워넣기, toggle 추가, refactor
+//TODO: refactor
 
 const MyInfo = () => {
   const { data: userInfo } = useQueryUserInfo();
@@ -15,8 +15,8 @@ const MyInfo = () => {
 
   return (
     <main className="relative flex h-full flex-col items-center bg-[#F2F2F2]">
-      <main className="container flex h-full w-full flex-col gap-9 pt-10">
-        <div className="flex w-full flex-col items-start gap-7 rounded-xl bg-white p-5">
+      <main className="container flex h-full w-full flex-col gap-9 pb-5 pt-10">
+        <div className="flex w-full flex-col items-start gap-5 rounded-xl bg-white p-5">
           {userInfo && (
             <div className="flex items-center gap-6">
               <div className="relative h-11 w-11">
@@ -41,29 +41,42 @@ const MyInfo = () => {
           </Button>
         </div>
         <section className="flex flex-col gap-5">
-          <div className="flex flex-col gap-2">
-            <div className="pl-3 text-sm font-bold">일반</div>
-            <div className="flex w-full flex-col gap-3 rounded-xl bg-white p-6">
-              <InfoItem title={"이름(입금자명)"} content={"김건국"} />
-              <InfoItem title={"전화번호"} content={"전화번호"} />
-              <InfoItem title={"사용자구분"} content={"대학생"} />
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <div className="pl-3 text-sm font-bold">학교</div>
-            <div className="flex w-full flex-col gap-3 rounded-xl bg-white p-6">
-              <InfoItem title={"학교"} content={"김건국"} />
-              <InfoItem title={"학과"} content={"전화번호"} />
-              <InfoItem title={"학번"} content={"대학생"} />
-              <InfoItem title={"학교 인증 여부"} content={"대학생"} />
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <div className="pl-3 text-sm font-bold">알림</div>
-            <div className="flex w-full flex-col gap-3 rounded-xl bg-white p-6">
-              <div className="text-xs font-semibold">푸시 알람 동의 여부</div>
-            </div>
-          </div>
+          {userInfo && (
+            <>
+              <div className="flex flex-col gap-2">
+                <div className="pl-3 text-sm font-bold">일반</div>
+                <div className="flex w-full flex-col gap-3 rounded-xl bg-white p-6">
+                  <InfoItem
+                    title={"이름(입금자명)"}
+                    content={userInfo.depositorName}
+                  />
+                  <InfoItem title={"전화번호"} content={userInfo.phoneNumber} />
+                  <InfoItem title={"사용자구분"} content={"대학생"} />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div className="pl-3 text-sm font-bold">학교</div>
+                <div className="flex w-full flex-col gap-3 rounded-xl bg-white p-6">
+                  <InfoItem title={"학교"} content={"userInfo.universityId"} />
+                  <InfoItem title={"학과"} content={userInfo.studentMajor} />
+                  <InfoItem title={"학번"} content={userInfo.studentCode} />
+                  <InfoItem
+                    title={"학교 인증 여부"}
+                    content={"userInfo.isRegistered"}
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div className="pl-3 text-sm font-bold">알림</div>
+                <div className="flex w-full items-center justify-between rounded-xl bg-white p-6">
+                  <div className="text-xs font-semibold">
+                    푸시 알람 동의 여부
+                  </div>
+                  <Switch />
+                </div>
+              </div>
+            </>
+          )}
         </section>
         <div className="flex flex-col gap-4">
           <Button
