69e80b586f695544d69d1ca0cd3c996473833b46
lunarmoon7 <zentechie7@gmail.com>
2024-06-25 10:17:31 +0900

    style(admin): 하단 Footer 생성

---
+++ apps/admin/components/Footer.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@ui/lib/utils";
+
+// TODO: 실제 링크로 대체
+const links = [
+  {
+    href: "/qr-read",
+    title: "QR 스캔하기",
+  },
+  {
+    href: "#축제",
+    title: "축제 정보 관리",
+  },
+  {
+    href: "#입금",
+    title: "입금 내역 확인",
+  },
+  {
+    href: "#예매",
+    title: "예매 내역 확인",
+  },
+];
+
+const Footer = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex h-14">
+      {links.map(({ href, title }) => (
+        <Link
+          href={href}
+          key={href}
+          className={cn(
+            "flex h-full items-center justify-center text-[10px] text-[#5E5E6E] px-4 grow",
+            pathname === href && "text-brand border-brand border-t-2 font-bold",
+          )}
+        >
+          {title}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Footer;
