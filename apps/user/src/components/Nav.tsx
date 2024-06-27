85d77e4ecc5c5c7da219d6fc0d232319829ec20a
SonSuBin129 <ttokky0129@gmail.com>
2024-06-27 13:46:05 +0900

    refactor: BackNav를 Nav로 합쳐서 관리

--- apps/user/src/components/Nav.tsx
+++ apps/user/src/components/Nav.tsx
@@ -1,6 +1,9 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FallbackProps } from "react-error-boundary";
 import { cn } from "@uket/ui/lib/utils";
+import { IconBack } from "@stackflow/plugin-basic-ui";
+
+import usePreviousPath from "@/hooks/usePreviousPath";
 
 import Profile from "./Profile";
 import Logo from "./Logo";
@@ -9,21 +12,30 @@ import RetryErrorBoundary from "./error/RetryErrorBoundary";
 
 const Nav = () => {
   const { pathname } = useLocation();
+  const previousPath = usePreviousPath();
 
   return (
     <nav className="container my-2 flex h-10 w-full items-center justify-between self-stretch">
-      <Logo />
-      <RetryErrorBoundary
-        resetKeys={["user-info"]}
-        fallbackComponent={(props: FallbackProps) => (
-          <LoginErrorFallback
-            className={cn("text-black", pathname === "/" && "text-white")}
-            {...props}
-          />
-        )}
-      >
-        <Profile />
-      </RetryErrorBoundary>
+      {["/", "/home"].includes(pathname) ? (
+        <>
+          <Logo />
+          <RetryErrorBoundary
+            resetKeys={["user-info"]}
+            fallbackComponent={(props: FallbackProps) => (
+              <LoginErrorFallback
+                className={cn("text-black", pathname === "/" && "text-white")}
+                {...props}
+              />
+            )}
+          >
+            <Profile />
+          </RetryErrorBoundary>
+        </>
+      ) : (
+        <Link to={previousPath}>
+          <IconBack />
+        </Link>
+      )}
     </nav>
   );
 };
--- apps/user/src/pages/_app.tsx
+++ apps/user/src/pages/_app.tsx
@@ -2,38 +2,20 @@ import { Outlet, useLocation } from "react-router-dom";
 
 import Nav from "@/components/Nav";
 import CriticalErrorBoundary from "@/components/error/CriticalErrorBoundary";
-import BackNav from "@/components/BackNav";
-
-import usePreviousPath from "@/hooks/usePreviousPath";
 
 import Redirects from "@/utils/redirects";
 
 const App = () => {
   const { pathname } = useLocation();
-  const previousPath = usePreviousPath();
-
-  const renderHeader = () => {
-    if (["/"].includes(pathname)) {
-      return null;
-    } else if (["/home"].includes(pathname)) {
-      return (
-        <header className="sticky left-0 top-0 z-10 bg-white">
-          <Nav />
-        </header>
-      );
-    } else {
-      return (
-        <header className="sticky left-0 top-0 z-10 bg-white">
-          <BackNav goURL={previousPath} />
-        </header>
-      );
-    }
-  };
 
   return (
     <CriticalErrorBoundary>
       <section className="relative flex h-dvh w-screen flex-col">
-        {renderHeader()}
+        {!["/"].includes(pathname) && (
+          <header className="sticky left-0 top-0 z-10 bg-white">
+            <Nav />
+          </header>
+        )}
         <main className="w-full grow">
           <Redirects>
             <Outlet />
