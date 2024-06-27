32435b832229bd6d505e5c2363d392f77e55df1f
SonSuBin129 <ttokky0129@gmail.com>
2024-06-27 16:17:19 +0900

    fix: myinfo에 retryerrorboundary 추가

--- apps/user/src/pages/myinfo/index.tsx
+++ apps/user/src/pages/myinfo/index.tsx
@@ -1,5 +1,8 @@
+import { Suspense } from "react";
 import { Switch } from "@uket/ui/components/ui/switch";
 import { Button } from "@uket/ui/components/ui/button";
+
+import RetryErrorBoundary from "@/components/error/RetryErrorBoundary";
 
 import { useQueryUserInfo } from "@/hooks/queries/useQueryUserInfo";
 
@@ -24,62 +27,66 @@ const MyInfo = () => {
   };
 
   return (
-    <main className="relative flex h-full flex-col items-center bg-[#F2F2F2]">
-      {userInfo && (
-        <main className="container flex h-full w-full flex-col gap-9 pb-5 pt-10">
-          <div className="flex w-full flex-col items-start gap-5 rounded-xl bg-white p-5">
-            <div className="flex items-center gap-6">
-              <div className="relative h-11 w-11">
-                <img
-                  src={userInfo.profileImage}
-                  alt="프로필 이미지"
-                  width={100}
-                  className="h-full w-full rounded-full object-cover"
+    <RetryErrorBoundary>
+      <Suspense>
+        <main className="relative flex h-full flex-col items-center bg-[#F2F2F2]">
+          {userInfo && (
+            <main className="container flex h-full w-full flex-col gap-9 pb-5 pt-10">
+              <div className="flex w-full flex-col items-start gap-5 rounded-xl bg-white p-5">
+                <div className="flex items-center gap-6">
+                  <div className="relative h-11 w-11">
+                    <img
+                      src={userInfo.profileImage}
+                      alt="프로필 이미지"
+                      width={100}
+                      className="h-full w-full rounded-full object-cover"
+                    />
+                  </div>
+                  <p className="text-xl font-bold">{userInfo.name}</p>
+                </div>
+                <Button
+                  variant="outline"
+                  className="w-full border-2 border-black py-2 text-center text-sm font-semibold"
+                  onClick={() => {
+                    navigate("/ticket-list");
+                  }}
+                >
+                  내 티켓 확인하기
+                </Button>
+              </div>
+              <section className="flex flex-col gap-5">
+                <GeneralInfo
+                  depositorName={userInfo.depositorName}
+                  phoneNumber={userInfo.phoneNumber}
                 />
+                <UnivInfo
+                  studentMajor={userInfo.studentMajor}
+                  studentCode={userInfo.studentCode}
+                />
+                <div className="flex flex-col gap-2">
+                  <div className="pl-3 text-sm font-bold">알림</div>
+                  <div className="flex w-full items-center justify-between rounded-xl bg-white p-6">
+                    <div className="text-xs font-semibold">
+                      푸시 알람 동의 여부
+                    </div>
+                    <Switch />
+                  </div>
+                </div>
+              </section>
+              <div className="flex flex-col items-center gap-4">
+                <LogoutModal onLogout={logout} />
+                <Button
+                  variant="secondary"
+                  className="w-full rounded-xl bg-white p-6 text-sm font-medium text-black hover:bg-slate-200 sm:w-80"
+                >
+                  회원탈퇴
+                </Button>
               </div>
-              <p className="text-xl font-bold">{userInfo.name}</p>
-            </div>
-            <Button
-              variant="outline"
-              className="w-full border-2 border-black py-2 text-center text-sm font-semibold"
-              onClick={() => {
-                navigate("/ticket-list");
-              }}
-            >
-              내 티켓 확인하기
-            </Button>
-          </div>
-          <section className="flex flex-col gap-5">
-            <GeneralInfo
-              depositorName={userInfo.depositorName}
-              phoneNumber={userInfo.phoneNumber}
-            />
-
-            <UnivInfo
-              studentMajor={userInfo.studentMajor}
-              studentCode={userInfo.studentCode}
-            />
-
-            <div className="flex flex-col gap-2">
-              <div className="pl-3 text-sm font-bold">알림</div>
-              <div className="flex w-full items-center justify-between rounded-xl bg-white p-6">
-                <div className="text-xs font-semibold">푸시 알람 동의 여부</div>
-                <Switch />
-              </div>
-            </div>
-          </section>
-          <div className="flex flex-col items-center gap-4">
-            <LogoutModal onLogout={logout} />
-            <Button
-              variant="secondary"
-              className="w-full rounded-xl bg-white p-6 text-sm font-medium text-black hover:bg-slate-200 sm:w-80"
-            >
-              회원탈퇴
-            </Button>
-          </div>
+            </main>
+          )}
         </main>
-      )}
-    </main>
+      </Suspense>
+    </RetryErrorBoundary>
   );
 };
 
