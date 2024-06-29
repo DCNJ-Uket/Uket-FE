b0f2229db6574f50b7dcca6b4e4d894f09797aac
SonSuBin129 <ttokky0129@gmail.com>
2024-06-29 23:45:43 +0900

    refactor: 확인 Modal Toast로 변경
    - 티켓 예매 취소 후 확인 모달 -> Toast로 변경

--- apps/user/src/hooks/mutations/useMutationCancelTicket.ts
+++ apps/user/src/hooks/mutations/useMutationCancelTicket.ts
@@ -1,13 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { CancelTicketResponse } from "@/types/ticketType";
 
 import { cancelTicket } from "@/api/ticket";
 
+
 export const useMutationCancelTicket = () => {
+  const queryClient = useQueryClient();
+
   const mutation = useMutation({
     mutationFn: (ticketId: number) => cancelTicket(ticketId),
     onSuccess: (data: CancelTicketResponse) => {
+      queryClient.invalidateQueries({ queryKey: ["my-ticket-list"] });
       return data;
     },
   });
