76a1670b017c1a89f65e43e2238b8700e0ce06b2
Subin, Son <81215596+SonSuBin129@users.noreply.github.com>
2024-07-01 14:38:26 +0900

    refactor: 확인 Modal Toast로 수정 (#65)

--- apps/user/src/pages/myinfo/_components/LogoutModal.tsx
+++ apps/user/src/pages/myinfo/_components/LogoutModal.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useToast } from "@uket/ui/components/ui/use-toast";
 import {
   Dialog,
   DialogClose,
@@ -16,76 +17,53 @@ interface LogoutModalProps {
 
 const LogoutModal = (props: LogoutModalProps) => {
   const { onLogout } = props;
+  const { toast } = useToast();
 
   const [open, setOpen] = useState(false);
-  const [logoutCompleted, setLogoutCompleted] = useState(false);
 
   const navigate = useNavigate();
 
   const handleLogout = () => {
     onLogout();
     setOpen(false);
-    setLogoutCompleted(true);
-  };
-
-  const handleComplete = () => {
-    setLogoutCompleted(false);
+    toast({
+      title: "로그아웃 성공!",
+    });
     navigate("/", { replace: true });
   };
 
   return (
-    <>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button
-            variant="secondary"
-            className="w-full rounded-xl bg-white p-6 text-sm font-medium text-black hover:bg-slate-200 sm:w-80"
-          >
-            로그아웃
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="max-w-60 rounded-2xl sm:max-w-xs">
-          <section className="pb-3 pt-5 sm:py-10">
-            <h1 className="text-center text-sm font-semibold">
-              로그아웃 하시겠습니까?
-            </h1>
-          </section>
-          <DialogFooter className="flex-row items-center justify-center gap-3">
-            <DialogClose asChild>
-              <Button
-                variant="destructive"
-                className="basis-1/2"
-                onClick={handleLogout}
-              >
-                네
-              </Button>
-            </DialogClose>
-            <Button className="basis-1/2" onClick={() => setOpen(false)}>
-              아니오
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="secondary"
+          className="w-full rounded-xl bg-white p-6 text-sm font-medium text-black hover:bg-slate-200 sm:w-80"
+        >
+          로그아웃
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-60 rounded-2xl sm:max-w-xs">
+        <section className="pb-3 pt-5 sm:py-10">
+          <h1 className="text-center text-sm font-semibold">
+            로그아웃 하시겠습니까?
+          </h1>
+        </section>
+        <DialogFooter className="flex-row items-center justify-center gap-3">
+          <DialogClose asChild>
+            <Button
+              variant="destructive"
+              className="basis-1/2"
+              onClick={handleLogout}
+            >
+              네
             </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-
-      {logoutCompleted && (
-        <Dialog open={logoutCompleted} onOpenChange={setLogoutCompleted}>
-          <DialogContent className="max-w-60 rounded-2xl sm:max-w-xs">
-            <section className="pb-3 pt-5 sm:py-10">
-              <h1 className="text-center text-sm font-semibold">
-                로그아웃이 완료되었습니다.
-              </h1>
-            </section>
-            <DialogFooter className="flex-row items-center justify-center gap-3">
-              <DialogClose asChild>
-                <Button className="mx-3 basis-full" onClick={handleComplete}>
-                  확인
-                </Button>
-              </DialogClose>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      )}
-    </>
+          </DialogClose>
+          <Button className="basis-1/2" onClick={() => setOpen(false)}>
+            아니오
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
