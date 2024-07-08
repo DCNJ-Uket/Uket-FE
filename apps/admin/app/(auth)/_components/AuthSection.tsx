be130b96d52dacaffd6b44e475c3424def97bae9
lunarmoon7 <zentechie7@gmail.com>
2024-07-08 21:59:45 +0900

    feat(admin): 로그인 기능에 react-hook-form과 zod 적용

--- apps/admin/app/(auth)/_components/AuthSection.tsx
+++ apps/admin/app/(auth)/_components/AuthSection.tsx
@@ -1,32 +1,77 @@
+"use client";
+
 import React from "react";
-import { Label } from "@ui/components/ui/label";
 import { Input } from "@ui/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@ui/components/ui/form";
 import { Button } from "@ui/components/ui/button";
 
+import { useLoginForm } from "@/hooks/useLoginForm";
+
 function AuthSection() {
+  const { form, onSubmit, error } = useLoginForm();
+
   return (
     <section className="flex grow flex-col justify-center gap-11">
       <header>
         <h1 className="text-2xl font-black">관리자 로그인</h1>
       </header>
       <main className="flex flex-col gap-10">
-        <div className="flex flex-col gap-3">
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="id" className="text-[#5E5E6E]">
-              ID
-            </Label>
-            <Input id="id" placeholder="아이디" />
-          </div>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="pw" className="text-[#5E5E6E]">
-              PW
-            </Label>
-            <Input id="pw" type="password" placeholder="비밀번호" />
-          </div>
-        </div>
-        <Button className="bg-brand hover:bg-brandHover w-full rounded-lg py-6 text-base md:max-w-24">
-          로그인
-        </Button>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="flex flex-col gap-12"
+          >
+            <div className="flex flex-col gap-3">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="grid w-full max-w-xs items-center gap-1.5">
+                    <FormLabel className="text-[#5E5E6E]">ID</FormLabel>
+                    <FormControl>
+                      <Input id="id" placeholder="아이디" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="grid w-full max-w-xs items-center gap-1.5">
+                    <FormLabel className="text-[#5E5E6E]">PW</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="pw"
+                        type="password"
+                        placeholder="비밀번호"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="space-y-4">
+              {error && <p className="text-center text-xs">{error}</p>}
+              <Button
+                type="submit"
+                className="bg-brand hover:bg-brandHover w-full rounded-lg py-6 text-base md:max-w-24"
+              >
+                로그인
+              </Button>
+            </div>
+          </form>
+        </Form>
       </main>
     </section>
   );
---
+++ apps/admin/hooks/useLoginForm.ts
@@ -0,0 +1,47 @@
+"use client";
+
+import { z } from "zod";
+import { UseFormReturn, useForm } from "react-hook-form";
+import { isAxiosError } from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LOGIN_ERROR } from "@/constants/error";
+
+import { useMutationLogin } from "./mutations/useMutationLogin";
+
+export type FormSchemaType = z.infer<typeof LoginFormSchema>;
+export type FormType = UseFormReturn<FormSchemaType, unknown, undefined>;
+
+const LoginFormSchema = z
+  .object({
+    email: z.string().email({ message: "유효하지 않은 이메일입니다." }),
+    password: z.string(),
+  })
+  .required();
+
+export const useLoginForm = () => {
+  const { mutate, error } = useMutationLogin();
+
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(LoginFormSchema),
+    mode: "onChange",
+  });
+
+  const onSubmit = async (data: FormSchemaType) => {
+    const { email, password } = data;
+
+    mutate({
+      email,
+      password,
+    });
+  };
+
+  return {
+    form,
+    onSubmit,
+    error:
+      error && isAxiosError(error)
+        ? LOGIN_ERROR[error.response?.status as keyof typeof LOGIN_ERROR]
+        : null,
+  };
+};
