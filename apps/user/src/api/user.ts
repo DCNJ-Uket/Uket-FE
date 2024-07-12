758a69804559a7e2aee1917b1d6dc7613270b31d
SonSuBin129 <ttokky0129@gmail.com>
2024-07-12 16:34:53 +0900

    feat: userinfo update api 연결 세팅

--- apps/user/src/api/user.ts
+++ apps/user/src/api/user.ts
@@ -1,7 +1,21 @@
+import { UserInfoResponse, UserInfoUpdateRequest } from "@/types/userType";
+
 import { instance } from "./instance";
 
 export const getUserInfo = async () => {
   const { data } = await instance.get("/users/info");
+
+  return data;
+};
+
+export const updateUserInfo = async ({
+  depositorName,
+  phoneNumber,
+}: UserInfoUpdateRequest) => {
+  const { data } = await instance.patch<UserInfoResponse>("/users/info", {
+    depositorName,
+    phoneNumber,
+  });
 
   return data;
 };
