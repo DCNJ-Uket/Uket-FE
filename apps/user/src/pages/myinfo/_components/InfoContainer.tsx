ceafbfff5739827f6599d4d18f07b2bf14c0306f
SonSuBin129 <ttokky0129@gmail.com>
2024-07-12 13:11:54 +0900

    style: 편집 버튼 클릭시 취소/저장 버튼

--- apps/user/src/pages/myinfo/_components/InfoContainer.tsx
+++ apps/user/src/pages/myinfo/_components/InfoContainer.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "@uket/ui/components/ui/button";
 
 interface InfoContainerProps {
@@ -9,17 +10,40 @@ interface InfoContainerProps {
 const InfoContainer = (props: InfoContainerProps) => {
   const { title, isGeneralInfo, children } = props;
 
+  const [isEdit, setIsEdit] = useState(false);
+  const handleIsEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
   return (
     <div className="flex w-full flex-col gap-2 bg-white px-6 pb-6 pt-4">
       <div className="flex h-8 items-center justify-between">
         <div className="text-lg font-bold text-[#17171B]">{title}</div>
-        {isGeneralInfo && (
+        {isGeneralInfo && !isEdit && (
           <Button
             variant="link"
-            className="cursor-pointer text-sm text-[#5E5E6E] underline"
+            className="cursor-pointer p-0 pr-px text-sm text-[#5E5E6E] underline"
+            onClick={handleIsEdit}
           >
             편집
           </Button>
+        )}
+        {isEdit && (
+          <div className="flex gap-6">
+            <Button
+              variant="link"
+              className="cursor-pointer p-0 pr-px text-sm  text-[#8989A1] underline"
+              onClick={handleIsEdit}
+            >
+              취소
+            </Button>
+            <Button
+              variant="link"
+              className="cursor-pointer p-0 pr-px text-sm text-[#724FFD] underline"
+            >
+              저장
+            </Button>
+          </div>
         )}
       </div>
       <div className="mb-3 w-full border-t-[0.3px] border-[#5E5E6E] opacity-50"></div>
