e64037c5313bdc2485817d4c406d067c40ea2c6f
SonSuBin129 <ttokky0129@gmail.com>
2024-07-14 18:12:57 +0900

    feat: usePreviousPath 수정
    - 이전 경로만 유지하는 것이 아니라 이전의 경로들 전부를 리스트로 관리하되 "/"이나 "/home" 일때는 초기화시켜주는 방향으로 수정

--- apps/user/src/hooks/usePreviousPath.ts
+++ apps/user/src/hooks/usePreviousPath.ts
@@ -1,21 +1,58 @@
+import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-const usePreviousPath = () => {
+const usePreviousPath = (): [string, () => void] => {
   const location = window.location.href;
+  const { pathname } = useLocation();
   const [previousPath, setPreviousPath] = useState<string>("/");
 
   useEffect(() => {
     const storage = globalThis?.sessionStorage;
     if (storage) {
-      const currentPath = storage.getItem("currentPath");
-      if (currentPath !== location) {
-        setPreviousPath(currentPath || "/");
-        storage.setItem("currentPath", location);
+      const pathStack: string[] = JSON.parse(
+        storage.getItem("pathStack") || "[]",
+      );
+
+      if (["/", "/home"].includes(pathname)) {
+        storage.setItem("pathStack", JSON.stringify([location]));
+        setPreviousPath(location);
+        return;
+      }
+
+      if (location.includes("/buy-ticket") && location.includes("showId")) {
+        return;
+      }
+
+      if (
+        pathStack.length === 0 ||
+        pathStack[pathStack.length - 1] !== location
+      ) {
+        setPreviousPath(pathStack[pathStack.length - 1]);
+        pathStack.push(location);
+        storage.setItem("pathStack", JSON.stringify(pathStack));
       }
     }
-  }, [location]);
+  }, [location, pathname]);
 
-  return previousPath;
+  const popPreviousPath = () => {
+    const storage = globalThis?.sessionStorage;
+    if (storage) {
+      const pathStack: string[] = JSON.parse(
+        storage.getItem("pathStack") || "[]",
+      );
+
+      if (pathStack.length > 1) {
+        pathStack.pop();
+        const prevPath = pathStack.pop() || "/";
+        setPreviousPath(prevPath);
+        storage.setItem("pathStack", JSON.stringify(pathStack));
+        return prevPath;
+      }
+    }
+    return "/";
+  };
+
+  return [previousPath, popPreviousPath];
 };
 
 export default usePreviousPath;
