eeb1cab7c89ffccc37f5e4d8e34c57a40fbccda5
lunarmoon7 <zentechie7@gmail.com>
2024-08-01 13:56:18 +0900

    fix: Image 컴포넌트 이미지 url 예외처리 추가
    - https이외에도 http를 추가했습니다.

--- apps/user/src/components/Image.tsx
+++ apps/user/src/components/Image.tsx
@@ -4,15 +4,16 @@ interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
 
 const Image = (props: ImageProps) => {
   const { src, ...rest } = props;
-  const isExternalImage = src!.startsWith("https") || src!.startsWith("blob");
-
+  const isExternalImage =
+    src!.startsWith("https") ||
+    src!.startsWith("http") ||
+    src!.startsWith("blob");
   const imgUrl =
     import.meta.env.MODE === "development"
       ? src
       : isExternalImage
         ? src
         : `${src}.webp`;
-
   const imgSrc = new URL(imgUrl || src!, import.meta.url).href;
 
   return <img src={imgSrc} {...rest} />;
