2f2c6175b56bfbe45e9dbee7d7130255d6d696dc
lunarmoon7 <zentechie7@gmail.com>
2024-08-01 13:02:17 +0900

    fix: Image 컴포넌트 imgUrl 할당 방식 변경

--- apps/user/src/components/Image.tsx
+++ apps/user/src/components/Image.tsx
@@ -7,9 +7,9 @@ const Image = (props: ImageProps) => {
   const imgUrl =
     import.meta.env.MODE === "development"
       ? src
-      : `@dist${src}.webp` !== undefined
-        ? `@dist${src}.webp`
-        : `@dist${src}`;
+      : `${src}.webp` !== undefined
+        ? `${src}.webp`
+        : `${src}`;
 
   const imgSrc = new URL(imgUrl || "", import.meta.url).href;
 
