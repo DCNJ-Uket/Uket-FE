574926d4731ad63d9276e4bcc25324505aaed8f1
SonSuBin129 <ttokky0129@gmail.com>
2024-10-13 22:55:58 +0900

    fix: 불필요한 조건문 제거

--- apps/admin/app/(main)/qr-scan/page.tsx
+++ apps/admin/app/(main)/qr-scan/page.tsx
@@ -1,5 +1,3 @@
-import { isBrowser } from "react-device-detect";
-
 import GreetingHeader from "@/components/GreetingHeader";
 
 import QRReaderSection from "./_components/QRReaderSection";
@@ -7,21 +5,8 @@ import QRReaderSection from "./_components/QRReaderSection";
 export default function QRReadPage() {
   return (
     <main className="flex h-full flex-col">
-      {isBrowser ? (
-        <>
-          <h1 className="py-5 pl-7 text-xl font-bold">QR 스캔하기</h1>
-          <section className="flex grow items-center justify-center">
-            <p className="text-xl">
-              QR 스캔하기 기능은 모바일 기기에서만 지원됩니다.
-            </p>
-          </section>
-        </>
-      ) : (
-        <>
-          <GreetingHeader />
-          <QRReaderSection />
-        </>
-      )}
+      <GreetingHeader />
+      <QRReaderSection />
     </main>
   );
 }
