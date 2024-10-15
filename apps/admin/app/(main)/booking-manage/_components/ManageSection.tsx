80a78ba02ad34174e154ae5bdda575626a6c4cef
SonSuBin129 <ttokky0129@gmail.com>
2024-10-15 21:15:00 +0900

    style: 검색 스타일 설정

--- apps/admin/app/(main)/booking-manage/_components/ManageSection.tsx
+++ apps/admin/app/(main)/booking-manage/_components/ManageSection.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import SearchSection from "./SearchSection";
 import BookingList from "./BookingList";
 
 function ManageSection() {
   return (
     <section className="flex grow flex-col gap-8 bg-[#F2F2F2] pl-16 pr-20 pt-20">
-      <div className="flex items-start justify-between">
+      <div className="flex items-center justify-between">
         <h1 className="text-[34px] font-bold">예매 내역 관리</h1>
-        <div>검색필드 들어올 부분</div>
+        <SearchSection />
       </div>
       <BookingList />
     </section>
