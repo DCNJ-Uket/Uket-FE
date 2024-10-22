5e0ab519c9fa72584da6afa27d77961d0ab330ce
coggiee <zentechie7@gmail.com>
2024-10-22 11:14:00 +0900

    feat: 로그인 유도 모달 리다이렉트 경로 수정
    - 로그인 시, 로그인 유도 모달을 연 페이지로 이동하도록 수정했습니다.

--- apps/user/src/hooks/mutations/useMutationLogin.ts
+++ apps/user/src/hooks/mutations/useMutationLogin.ts
@@ -3,7 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 import { AuthResponse, LoginRequestParams } from "@/types/authType";
 
 import { setAccessToken } from "@/utils/handleToken";
-import { setRefreshToken } from "@/utils/handleCookie";
+import { deleteCookie, getCookie, setRefreshToken } from "@/utils/handleCookie";
 
 import { useNavigate } from "@/router";
 import { login } from "@/api/auth";
@@ -19,9 +19,21 @@ export const useMutationLogin = () => {
       setRefreshToken("refreshToken", refreshToken);
 
       if (isRegistered) {
-        navigate("/", {
-          replace: true,
-        });
+        const login_redirect_url = getCookie("login_redirect_url");
+        if (login_redirect_url) {
+          deleteCookie("login_redirect_url", {
+            path: "/",
+          });
+
+          navigate(login_redirect_url, {
+            replace: true,
+          });
+          return;
+        } else {
+          navigate("/", {
+            replace: true,
+          });
+        }
       } else {
         navigate("/signup", {
           state: { isUnRegistered: true },
--- apps/user/src/utils/handleCookie.ts
+++ apps/user/src/utils/handleCookie.ts
@@ -13,6 +13,27 @@ export interface CookieSetOptions {
 
 const cookies = new Cookies();
 
+export const setCookie = (
+  name: string,
+  value: string,
+  options?: CookieSetOptions,
+) => {
+  const defaultOptions: CookieSetOptions = {
+    maxAge: 60 * 60 * 2,
+    path: "/",
+  };
+
+  cookies.set(name, value, { ...defaultOptions, ...options });
+};
+
+export const getCookie = (name: string) => {
+  return cookies.get(name);
+};
+
+export const deleteCookie = (name: string, options?: CookieSetOptions) => {
+  cookies.remove(name, { ...options });
+};
+
 export const setRefreshToken = (
   name: string,
   value: string,
