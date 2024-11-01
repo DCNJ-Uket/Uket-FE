615a77426330045c0e6dba4ec43d6b23d32c8665
coggiee <zentechie7@gmail.com>
2024-11-01 10:59:41 +0900

    refactor(admin): queryClient 설정 변경

--- apps/admin/app/providers.tsx
+++ apps/admin/app/providers.tsx
@@ -1,56 +1,33 @@
-// In Next.js, this file would be called: app/providers.jsx
 "use client";
 
-// Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
+import { useState } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
-  isServer,
   HydrationBoundary,
   QueryClient,
   QueryClientProvider,
   dehydrate,
 } from "@tanstack/react-query";
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        throwOnError: true,
-        retry: false,
-        staleTime: 60 * 1000,
-      },
-      mutations: {
-        throwOnError: true,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    // Server: always make a new query client
-    return makeQueryClient();
-  } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
-
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // NOTE: Avoid useState when initializing the query client if you don't
-  //       have a suspense boundary between this and the code that may
-  //       suspend because React will throw away the client on the initial
-  //       render if it suspends and there is no boundary
-  const queryClient = getQueryClient();
-
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnMount: false,
+            refetchOnWindowFocus: false,
+            refetchInterval: false,
+            staleTime: 1000 * 60 * 10,
+            retry: false,
+            throwOnError: true,
+          },
+          mutations: {
+            throwOnError: false,
+          },
+        },
+      }),
+  );
   return (
     <QueryClientProvider client={queryClient}>
       <HydrationBoundary state={dehydrate(queryClient)}>
