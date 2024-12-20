ab25521f2dadd29648110cc9a1da8f046e981a2b
coggiee <zentechie7@gmail.com>
2024-12-20 11:55:16 +0900

    chore: puppeteer vercel 에러

--- apps/soritor/vite.config.ts
+++ apps/soritor/vite.config.ts
@@ -6,6 +6,7 @@ import imageminPngQuant from "imagemin-pngquant";
 import react from "@vitejs/plugin-react";
 import viteImagemin from "@vheemstra/vite-plugin-imagemin";
 import prerender from "@prerenderer/rollup-plugin";
+import puppeteerRenderer from "@prerenderer/renderer-puppeteer";
 import generouted from "@generouted/react-router/plugin";
 
 // https://vitejs.dev/config/
@@ -26,7 +27,7 @@ export default defineConfig({
     }),
     prerender({
       routes: ["/", "/select-univ", "/login"],
-      renderer: "@prerenderer/renderer-puppeteer",
+      renderer: puppeteerRenderer,
       rendererOptions: {
         maxConcurrentRoutes: 1,
         renderAfterTime: 500,
