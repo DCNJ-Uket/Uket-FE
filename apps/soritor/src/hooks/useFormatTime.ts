17d436d9525b91c9c4801e9c3e7c791464748aab
SonSuBin129 <ttokky0129@gmail.com>
2025-01-01 17:07:58 +0900

    fix: 티켓예매 시간대 변경

--- apps/soritor/src/hooks/useFormatTime.ts
+++ apps/soritor/src/hooks/useFormatTime.ts
@@ -1,8 +1,18 @@
 export const useFormatTime = (date: string) => {
-  const datePart = date.split("T")[0];
-  const formatDate = datePart.replace(/-/g, ".");
+  const timeOffset = date.split("+")[1];
+
+  const [offsetHours, offsetMinutes] = String(timeOffset)
+    .split(":")
+    .map(Number);
 
   const dateObject = new Date(date);
+  dateObject.setHours(dateObject.getHours() + offsetHours);
+  dateObject.setMinutes(dateObject.getMinutes() + offsetMinutes);
+
+  const dateYear = String(dateObject.getFullYear());
+  const dateMonth = String(dateObject.getMonth() + 1).padStart(2, "0");
+  const dateDay = String(dateObject.getDate()).padStart(2, "0");
+  const formatDate = `${dateYear}. ${dateMonth}. ${dateDay}`;
 
   const dateHours = String(dateObject.getHours()).padStart(2, "0");
   const dateMinutes = String(dateObject.getMinutes()).padStart(2, "0");
