7f880f6b8996f1e39d925f999d442f83bef371ce
coggiee <zentechie7@gmail.com>
2025-01-17 13:57:36 +0900

    refactor: errorHandler 변경

--- apps/soritor/src/utils/errorHandler.ts
+++ apps/soritor/src/utils/errorHandler.ts
@@ -2,16 +2,11 @@ import { toast } from "@uket/ui/components/ui/sonner";
 
 import CustomAxiosError from "./customError";
 
-export const useHandleError = () => {
-  // 토스트를 띄워줍니다.
-  const errorHandler = (error: CustomAxiosError) => {
-    if (error.isToast) {
-      toast.error(`${error.errorContent?.title || "에러가 발생했어요"}`, {
-        description:
-          error.errorContent?.description || "잠시 후 시도해 주세요.",
-      });
-    }
-  };
-
-  return { errorHandler };
+// 토스트를 띄워줍니다.
+export const errorHandler = (error: CustomAxiosError) => {
+  if (error.isToast) {
+    toast.error(`${error.errorContent?.title || "에러가 발생했어요"}`, {
+      description: error.errorContent?.description || "잠시 후 시도해 주세요.",
+    });
+  }
 };
